c73d5ab0a50f84d2a5da7b672131a0e0301df10c
Jared Hanson <jaredhanson@gmail.com>
2017-12-17 14:18:11 -0800

    Remove Broker.

--- app/broker/amqp.js
+++
@@ -1,6 +0,0 @@
-exports = module.exports = function() {
-  
-  return function amqp() {
-    return 'http://i.bixbyjs.org/ms/net/amqp/Broker';
-  };
-};
--- app/broker/google.js
+++
@@ -1,6 +0,0 @@
-exports = module.exports = function() {
-  
-  return function gcp() {
-    return 'http://i.bixbyjs.org/ms/opt/gcp/pubsub/Broker';
-  };
-};
